61845e298dd1691cbf5fc7e7fd936c131aff2f3d
hanrenguang <413526182@qq.com>
2017-06-13 18:28:38 +0800

    完成报文的解析

--- server.py
+++ server.py
@@ -1,44 +1,72 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import socket, time
+import socket
 
+# 资源根目录
 root = '.'
 
-def getResponse(contentTp, path):
-	Eol = "\r\n"
-	spc = " "
-	prot = "HTTP/1.1"
-	status = "200"
-	statusCode = "OK"
-	htmlContentType = "text/html; charset=utf-8"
-	cssContentType = "text/css"
-	jsContentType = "application/javascript"
-	contentType = ""
-	content = ''
+# def getResponse(contentTp, path):
+# 	Eol = "\r\n"
+# 	spc = " "
+# 	prot = "HTTP/1.1"
+# 	status = "200"
+# 	statusCode = "OK"
+# 	htmlContentType = "text/html; charset=utf-8"
+# 	cssContentType = "text/css"
+# 	jsContentType = "application/javascript"
+# 	contentType = ""
+# 	content = ''
 
-	if contentTp == 'html':
-		contentType = htmlContentType
-	elif contentTp == 'css':
-		contentType = cssContentType
-	elif contentTp == 'js':
-		contentType = jsContentType
+# 	if contentTp == 'html':
+# 		contentType = htmlContentType
+# 	elif contentTp == 'css':
+# 		contentType = cssContentType
+# 	elif contentTp == 'js':
+# 		contentType = jsContentType
 
-	try:
-		f = open(root + path, 'r')
-		while True:
-			chunk = f.read(1024)
-			if not chunk:
-				f.close()
-				break;
-			content += chunk
-	except:
-		pass
+# 	try:
+# 		f = open(root + path, 'r')
+# 		while True:
+# 			chunk = f.read(1024)
+# 			if not chunk:
+# 				f.close()
+# 				break;
+# 			content += chunk
+# 	except:
+# 		pass
 
-	ret = prot + spc + status + spc + statusCode + Eol + "Content-Length: " + str(len(content)) + Eol + "Content-Type: " + contentType + Eol + Eol + content
+# 	ret = prot + spc + status + spc + statusCode + Eol + "Content-Length: " + str(len(content)) + Eol + "Content-Type: " + contentType + Eol + Eol + content
 
-	return ret
+# 	return ret
 
+# 解析请求报文
+def parseReq(reqList):
+	# 保存解析结果
+	parseRet = {}
+
+	method = reqList[0].split(' ')[0]
+	sourcePath = reqList[0].split(' ')[1]
+
+	parseRet['method'] = method
+	parseRet['sourcePath'] = sourcePath
+
+	i = len(reqList) - 1
+
+	while i:
+		if len(reqList[i].split(':')) == 1:
+			i = i - 1
+			continue
+
+		idx = reqList[i].find(':')
+		key, value = reqList[i][0:idx], reqList[i][idx+1:]
+		parseRet['{0}'.format(key)] = value.strip()
+		i = i - 1
+
+	return parseRet
+
+
+# 开启服务器
 def serve():
 	sk = socket.socket(
 		socket.AF_INET, 
@@ -55,12 +83,15 @@ def serve():
 		try:
 			clientSk, addr = sk.accept()
 			print("address is: %s" % str(addr))
-			ret = clientSk.recv(1024).decode().split("\r\n")
-			requestSource = ret[0].split(" ")[1]
-			contentTp = requestSource.split(".")[1]
-			clientSk.sendall(getResponse(contentTp, requestSource).encode(encoding='UTF-8'))
+
+			requestList = clientSk.recv(1024).decode().split("\r\n")
+			# 解析HTTP请求报文
+			ret = parseReq(requestList)
+
+			# 返回HTTP响应报文
+			# clientSk.sendall(response.encode(encoding='UTF-8'))
 			clientSk.close()
-			time.sleep(1)
+
 		except Exception as err:
 			print(err)
 			clientSk.close()
